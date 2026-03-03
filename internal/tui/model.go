f603a60f9bd5ee624f4c57413fc9a68d73e523c0
bishalr0y <bishalroy895@gmail.com>
2026-03-03 22:19:48 +0530

    feat: add help view

--- internal/tui/model.go
+++ internal/tui/model.go
@@ -3,6 +3,8 @@ package tui
 import (
 	"strconv"
 
+	"charm.land/bubbles/v2/help"
+	"charm.land/bubbles/v2/key"
 	"charm.land/bubbles/v2/table"
 	tea "charm.land/bubbletea/v2"
 	"charm.land/lipgloss/v2"
@@ -13,15 +15,64 @@ var baseStyle = lipgloss.NewStyle().
 	BorderStyle(lipgloss.NormalBorder()).
 	BorderForeground(lipgloss.Color("240"))
 
+type keyMap struct {
+	Up      key.Binding
+	Down    key.Binding
+	Kill    key.Binding
+	Refresh key.Binding
+	Quit    key.Binding
+}
+
+// ShortHelp returns keybindings to be shown in the mini help view
+func (k keyMap) ShortHelp() []key.Binding {
+	return []key.Binding{k.Up, k.Down, k.Quit, k.Kill, k.Refresh}
+}
+
+// FullHelp returns keybindings for the expaned help view
+func (k keyMap) FullHelp() [][]key.Binding {
+	return [][]key.Binding{
+		{k.Up, k.Down},      // first column
+		{k.Kill, k.Refresh}, // second column
+		{k.Quit},            // third column
+	}
+}
+
+var keys = keyMap{
+	Up: key.NewBinding(
+		key.WithKeys("up", "k"),
+		key.WithHelp("↑/k", "move up"),
+	),
+	Down: key.NewBinding(
+		key.WithKeys("down", "j"),
+		key.WithHelp("↓/j", "move down"),
+	),
+	Kill: key.NewBinding(
+		key.WithKeys("k"),
+		key.WithHelp("k", "kill process"),
+	),
+	Refresh: key.NewBinding(
+		key.WithKeys("r"),
+		key.WithHelp("r", "refresh"),
+	),
+	Quit: key.NewBinding(
+		key.WithKeys("q", "esc", "ctrl+c"),
+		key.WithHelp("q", "quit"),
+	),
+}
+
 type model struct {
 	table     table.Model
 	processes []process.Process
+	keys      keyMap
+	help      help.Model
 }
 
 func NewModel(table table.Model, processes []process.Process) *model {
 	return &model{
 		table:     table,
 		processes: processes,
+		keys:      keys,
+		help:      help.New(),
 	}
 }
 
@@ -76,5 +127,8 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 }
 
 func (m model) View() tea.View {
-	return tea.NewView(baseStyle.Render(m.table.View()) + "\n  " + m.table.HelpView() + "\n")
+	helpView := m.help.View(m.keys)
+	// return tea.NewView(baseStyle.Render(m.table.View()) + "\n  " + m.table.HelpView() + "\n")
+
+	return tea.NewView(baseStyle.Render(m.table.View()) + "\n  " + helpView + "\n")
 }
