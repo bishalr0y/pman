fc68c2d3bdfeb4c3bde8ea8ce15ba123346649fc
bishalr0y <bishalroy895@gmail.com>
2026-03-20 15:10:14 +0530

    docs: update version command description

--- cmd/version.go
+++ cmd/version.go
@@ -15,13 +15,8 @@ const Version = "0.1.0"
 // versionCmd represents the version command
 var versionCmd = &cobra.Command{
 	Use:   "version",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Display the pman version",
+	Long:  `Print the version number and build information of pman.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Printf("pman version: %s\n", Version)
 	},
